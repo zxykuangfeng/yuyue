37816651eebac7d73b978f86323d5bf86d0a1ce4
18036291231@163.com <18036291231@163.com>
2025-11-10 17:35:45 +0800

    提交

---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="container">
+		
+				<button type="default" @click="goto('/pages/about/about')">通过方法跳转到about页面</button>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
